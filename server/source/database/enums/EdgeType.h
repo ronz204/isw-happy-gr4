79d6cae67cea8e439f92808b2c3906972221554e
roney ruiz <roneyruizrojas@gmail.com>
2026-04-21 09:34:10 -0600

    fix(server): add elevator edte type

--- server/source/database/enums/EdgeType.h
+++ server/source/database/enums/EdgeType.h
@@ -7,6 +7,7 @@ enum class EdgeType
 {
   Walking = 0,
   Stairs = 1,
+  Elevator = 2,
 };
 
 // Convert EdgeType to string
@@ -16,6 +17,7 @@ inline std::string toString(EdgeType type)
   {
   case EdgeType::Walking: return "Walking";
   case EdgeType::Stairs: return "Stairs";
+  case EdgeType::Elevator: return "Elevator";
   default:
     throw std::invalid_argument("Invalid EdgeType");
   }
@@ -26,6 +28,7 @@ inline EdgeType edgeTypeFromString(const std::string &str)
 {
   if (str == "Walking") return EdgeType::Walking;
   if (str == "Stairs") return EdgeType::Stairs;
+  if (str == "Elevator") return EdgeType::Elevator;
   throw std::invalid_argument("Invalid EdgeType string: " + str);
 }
 
