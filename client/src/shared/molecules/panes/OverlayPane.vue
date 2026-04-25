928a0fa5b2819a51f7d4540837e0cb2340fcbe53
roney ruiz <roneyruizrojas@gmail.com>
2026-04-24 22:24:34 -0600

    feat(client): complete overlay pane

---
+++ client/src/shared/molecules/panes/OverlayPane.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Bounds } from "@domain/zix";
+import type { UseViewportReturn } from "@domain/hooks";
+
+import { inject, watch } from "vue";
+
+const props = defineProps<{
+  imagePath: string
+  bounds: Bounds
+}>()
+
+const viewport = inject<UseViewportReturn>('zixViewport')
+watch(() => props.bounds, (b) => { viewport?.setBounds(b) }, { immediate: true })
+</script>
+
+<template>
+  <img :src="imagePath" class="block pointer-events-none" />
+</template>
