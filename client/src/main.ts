004a8768c859f66af49f3d4f8663664acb74885d
roney ruiz <roneyruizrojas@gmail.com>
2026-04-24 21:39:55 -0600

    feat(client): move taiwlind and nuxt import to assets

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
-import "./style.css";
 import App from "./App.vue";
 import { createApp } from "vue";
+import "@assets/styles/main.css";
 
 import { router } from "./router";
 import ui from "@nuxt/ui/vue-plugin";
